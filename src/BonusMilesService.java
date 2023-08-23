e4395b27ceb6b69cfa8fafa228ca4d82e0f2f9f7
Sanan <mamedli.sanan2018@gmail.com>
2023-08-23 21:46:38 +0300

    changed MAIN

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate(int price) {
+        price = 10000;
+        int calculate;
+        calculate = price / 20;
+        return calculate;
+    }
+
+}
